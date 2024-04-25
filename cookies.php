be50e5b37bb23c7d70d87c88362849509d33fe70
apoorvakhajbage <apoorvauk24@gmail.com>
2024-04-25 10:13:11 +0530

    new changes

--- cookies.php
+++ cookies.php
@@ -5,7 +5,7 @@ session_start();
 $con = mysqli_connect('localhost', 'root', '', 'registration');
 
 // Check connection
-if (mysqli_connect_errno()) {
+if (!$con) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     exit();
 }
